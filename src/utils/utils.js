29d2387c898b8b628403cffbbda8f0881e76837e
Oleg Futurecat <vtoroysite1@yandex.ru>
2021-05-01 15:55:19 +0300

    сделать срочные исправления

--- src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export function requestLoading(button, isLoading, text) {
-    if (isLoading) {
-      button.setAttribute('disabled', true)
-      button.textContent = text;
-    } else {
-      button.removeAttribute('disabled', false)
-      button.textContent = text;
-    }
-  }
